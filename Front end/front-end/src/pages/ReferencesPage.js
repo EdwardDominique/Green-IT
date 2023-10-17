80768aedf566844aade6f48afcbcbaf2a9153aae
Aurelien95300 <manceau.aurelien@laposte.net>
2023-10-18 00:26:15 +0200

    page de reference

--- Front end/front-end/src/pages/ReferencesPage.js
+++ Front end/front-end/src/pages/ReferencesPage.js
@@ -1,17 +1,54 @@
-// pages/ReferencesPage.js
-import React from "react";
+import React from 'react';
+import './ReferencesPage.css';
 
-function ReferencesPage() {
-  return (
-    <div>
-      <h1>Références</h1>
-      <ul>
-        <li><a href="https://example.com" target="_blank" rel="noopener noreferrer">Référence 1</a></li>
-        <li><a href="https://example.com" target="_blank" rel="noopener noreferrer">Référence 2</a></li>
-      </ul>
-    </div>
-  );
+function Reference() {
+    // Un exemple de données pour les références
+    const references = [
+        {
+            titre: 'ref1',
+            imgSrc: 'image.jpg',
+            description: 'une description',
+            lien: ''
+        },
+        {
+            titre: 'ref2',
+            imgSrc: 'image.jpg',
+            description: 'une description',
+            lien: ''
+        },
+        {
+            titre: 'ref3',
+            imgSrc: 'image.jpg',
+            description: 'une description',
+            lien: ''
+        },
+        {
+            titre: 'ref4',
+            imgSrc: 'image.jpg',
+            description: 'une description',
+            lien: ''
+        },
+        //... Ajoutez autant de références que nécessaire
+    ];
+
+    return (
+        <div className="reference-page">
+            <h1>Page de référence pour le Green IT</h1>
+            <section className="introduction">
+                <p>Cette page a pour objectif de rediriger vers des sites proposant des solutions pour le Green IT, afin d'encourager une informatique plus responsable et durable.</p>
+            </section>
+            <div className="references-grid">
+                {references.map((reference, index) => (
+                    <div key={index} className="reference-item">
+                        <img src={reference.imgSrc} alt={reference.titre} />
+                        <h2>{reference.titre}</h2>
+                        <p>{reference.description}</p>
+                        <a href={reference.lien}>Découvrir</a>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default ReferencesPage;
-
+export default Reference;
