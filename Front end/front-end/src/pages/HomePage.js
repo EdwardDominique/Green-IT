df7f7cbb9ffb01b90c340549c808faeb5bb04a97
Leonard Bonneville <leonard.bonneville76@gmail.com>
2023-10-18 00:45:22 +0200

    amelioration visuel accueil

--- Front end/front-end/src/pages/HomePage.js
+++ Front end/front-end/src/pages/HomePage.js
@@ -14,12 +14,12 @@ function HomePage() {
             </section>
 
             <section id="student-projects">
-                <h2>Projets d'étudiants en informatique :</h2>
+                <h1>Projets d'étudiants en informatique :</h1>
                 <p>Découvrez une variété de projets innovants élaborés par des étudiants en ingénierie informatique. Chaque projet présenté vise à résoudre des problématiques environnementales à l'aide de solutions technologiques. De l'optimisation énergétique à la conception de plateformes éco-responsables, laissez-vous inspirer par le talent et la créativité de ces jeunes ingénieurs.</p>
             </section>
 
             <section id="references">
-                <h2>Références vers d'autres sites sur le Green IT :</h2>
+                <h1>Références vers d'autres sites sur le Green IT :</h1>
                 <p>La technologie verte est un domaine en constante évolution, avec de nouvelles découvertes et initiatives chaque jour. Nous vous invitons à explorer d'autres ressources pour enrichir vos connaissances sur le sujet. Découvrez notre sélection de sites web dédiés au Green IT, qui offrent des perspectives, des études de cas, et des innovations dans ce domaine passionnant.</p>
             </section>
 
