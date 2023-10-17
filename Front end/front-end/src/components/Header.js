a8b35f4f29b844f111ca5aacac705f9a18d6622e
Aurelien95300 <manceau.aurelien@laposte.net>
2023-10-18 00:30:44 +0200

    changement dans le header

--- Front end/front-end/src/components/Header.js
+++ Front end/front-end/src/components/Header.js
@@ -1,15 +1,11 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Button, IconButton } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton size="large" edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
         <Typography variant="h6" style={{ flexGrow: 1 }}>
           Green [it]
         </Typography>
