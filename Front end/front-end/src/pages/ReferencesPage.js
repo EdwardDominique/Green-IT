75a22d0ebc7d4770db6872be3ae6e56d3c131927
bassi-tech <hbassidiki@gmail.com>
2023-10-25 16:51:15 +0200

    Modif ref14 et nom article

--- Front end/front-end/src/pages/ReferencesPage.js
+++ Front end/front-end/src/pages/ReferencesPage.js
@@ -23,85 +23,85 @@ function Reference() {
             titre: 'Le Green IT, une démarche pleine d’ambitions et d’opportunités pour les entreprises',
             imgSrc: ref1Image,
             description: 'Le Green IT est une démarche visant à réduire l’impact environnemental du numérique, qui représente une part significative des émissions mondiales de gaz à effet de serre. Cette démarche englobe divers aspects, allant de la réduction de la consommation d’énergie des infrastructures numériques à l’éco-conception des produits et services numériques. En plus de ses bénéfices environnementaux, le Green IT offre également de nombreuses opportunités pour les entreprises en termes d’efficacité et d’innovation.',
-            auteur: 'Review par : Henrique MARTINS'
+            auteur: 'Henrique MARTINS'
         },
         {
             titre: 'Analyse Énergétique et Environnementale des Navigateurs Web Android en 2021',
             imgSrc: ref2Image,
             description: "L'article analyse la consommation énergétique et l'impact environnemental de 16 navigateurs web pour Android en 2021. Firefox Focus se distingue par sa faible consommation énergétique, tandis que des navigateurs populaires comme Edge, Firefox, et Chrome présentent des performances moindres à cet égard.",
-            auteur: 'Review par : Aurélien MANCEAU'
+            auteur: 'Aurélien MANCEAU'
         },
         {
             titre: 'Évaluation des Technologies Blockchain pour la Gestion des Transactions et des Actifs en Utilisant les Principes de Green IT',
             imgSrc: ref3Image,
             description: "L'étude évalue la technologie Blockchain pour gérer les transactions et les actifs en se basant sur les principes de Green IT. Des améliorations notables ont été observées, notamment une réduction de la consommation d'énergie de 30%, une augmentation de l'efficacité du traitement de 20% et une réduction des émissions de CO2 de 25%. Ces améliorations ont été obtenues grâce à l'optimisation des algorithmes de consensus, l'utilisation de matériel éco-énergétique, et l'emploi d'énergies renouvelables.",
-            auteur: 'Review par : Léonard BONNEVILLE'
+            auteur: 'Léonard BONNEVILLE'
         },
         {
             titre: 'Le Green Coding : Vers une Informatique Plus Durable',
             imgSrc: ref4Image,
             description: "La demande mondiale en électricité a augmenté avec l'évolution de la programmation informatique. Pourtant, le green coding émerge comme une solution pour réduire la consommation énergétique des technologies de l'information. Voici les principales solutions mises en avant pour une programmation plus respectueuse de l'environnement.",
-            auteur: 'Review par : Aurélien MANCEAU'
+            auteur: 'Aurélien MANCEAU'
         },
         {
             titre: 'Évaluation de la Green IT et des Pratiques de Programmation Écoénergétiques',
             imgSrc: ref5Image,
             description: "Le rapport évalue l'impact environnemental du développement logiciel, explore l'efficacité énergétique des langages de programmation, et analyse les attitudes des programmeurs envers la Green IT. Des langages comme C et Rust sont identifiés comme écoénergétiques, et des outils comme PageSpeed Insights et GraphQL sont présentés comme des moyens efficaces d'améliorer l'efficacité énergétique du développement web.",
-            auteur: 'Review par : Léonard BONNEVILLE'
+            auteur: 'Léonard BONNEVILLE'
         },
         {
             titre: 'AI for Green : la branche verte de l’intelligence artificielle',
             imgSrc: ref6Image,
             description: "Le Green IT et l'IA sont deux domaines en pleine expansion qui ont un impact significatif sur notre environnement. Bien que l'IA puisse contribuer à l'augmentation des émissions de gaz à effet de serre, elle peut également être utilisée pour aider à la transition écologique. Le concept de Green IA, qui vise à rendre l'IA plus durable, est une initiative prometteuse dans ce sens.",
-            auteur: 'Review par : Henrique MARTINS'
+            auteur: 'Henrique MARTINS'
         },
         {
             titre: 'Efficacité énergétique des langages de programmation',
             imgSrc: ref7Image,
             description: "Dans cet article informatif, nous explorons le domaine du Green IT et son impact sur l'efficacité énergétique des langages de programmation. À travers une étude détaillée portant sur 27 langages populaires, nous découvrons ceux qui se distinguent en matière de durabilité. De la méthodologie aux résultats, nous mettons en lumière l'impact environnemental des choix de langage. En fin de compte, nous tirons des conclusions importantes qui éclairent le rôle du Green IT dans l'industrie de la programmation pour un avenir plus respectueux de l'environnement.",
-            auteur: 'Review par : Bassidiki HAIDARA'
+            auteur: 'Bassidiki HAIDARA'
         },
         {
-            titre: 'GREEN CLOUD COMPUTING AND ENVIRONMENTAL SUSTAINABILITY',
+            titre: 'CLOUD INFORMATIQUE VERT et DURABILITÉ ENVIRONNEMENTALE',
             imgSrc: ref8Image,
             description: "Le cloud informatique est devenue une infrastructure essentielle pour les entreprises, mais sa croissance rapide soulève des préoccupations environnementales. L'article se concentre sur la consommation d'énergie des centres de données, devenue un problème critique en raison des coûts opérationnels élevés et des émissions de carbone importantes. Pour résoudre ces problèmes, l'article explore des solutions pour rendre le cloud informatique plus respectueuse de l'environnement. Il examine les aspects technologiques qui contribuent à la consommation d'énergie et propose des orientations pour la recherche future dans le domaine de le cloud informatique vert.",
-            auteur: 'Review par : Flavien GAUTIER'
+            auteur: 'Flavien GAUTIER'
         },
         {
             titre: 'IBM Cloud Carbon Calculator Aims to Aid Enterprise Sustainability Efforts',
             imgSrc: ref9Image,
             description: "IBM utilise son expertise en matière d'IA pour améliorer les initiatives de développement durable en aidant les entreprises à mieux comprendre l'impact de l'informatique dématérialisée sur l'environnement.",
-            auteur: 'Review par : Maxime MOREL'
+            auteur: 'Maxime MOREL'
         },
         {
             titre: 'A sustainable and secure load management model for green cloud data centres',
             imgSrc: ref10Image,
             description: "Un modèle SaS-LM propose une gestion de charge durable et sécurisée pour les centres de données cloud. Il utilise l'optimisation DPBHO pour réduire les émissions de carbone, la consommation d'énergie et améliorer l'utilisation des ressources.",
-            auteur: 'Review par : Maxime MOREL'
+            auteur: 'Maxime MOREL'
         },
         {
             titre: 'Resource-aware Programming in a High-level Language',
             imgSrc: ref11Image,
             description: "La programmation tenant compte des ressources dans les langages de haut niveau sur les MPSoC est réalisable avec un effort gérable et améliore les performances.",
-            auteur: 'Review par : Maxime MOREL'
+            auteur: 'Maxime MOREL'
         },
         {
             titre: 'Making software and data architectures more sustainable',
             imgSrc: ref12Image,
             description: "Les éditeurs de logiciels ont la possibilité de réduire les émissions de carbone des applications en réévaluant les approches traditionnelles et en intégrant dès le départ la notion de durabilité.",
-            auteur: 'Review par : Maxime MOREL'
+            auteur: 'Maxime MOREL'
         },
         {
             titre: 'Telecom SudParis lance un programme de formation pour former les ingénieurs du Green IT',
             imgSrc: ref13Image,
             description: "Cet article parle de l'initiative de Telecom SudParis, une école d'ingénieurs en France, pour former les ingénieurs du futur spécialisés dans les technologies vertes. L'école a lancé un nouveau programme de formation appelé 'Green IT' qui vise à former des étudiants à la conception et à la mise en œuvre de solutions informatiques durables pour réduire l'impact environnemental des technologies de l'information. Le programme comprend des cours sur la conception de centres de données économes en énergie, la gestion de l'énergie et la réduction de l'empreinte carbone des infrastructures informatiques. Telecom SudParis espère ainsi répondre à la demande croissante de professionnels qualifiés dans le domaine de l'informatique verte.",
-            auteur: 'Review par : Henrique DONG'
+            auteur: 'Henrique DONG'
         },
         {
             titre: 'BENCHMARK GREEN IT 2022',
             imgSrc: ref14Image,
             description: "L'article 'Benchmark Green IT 2022' souligne l'importance de l'Analyse du Cycle de Vie (ACV) dans le domaine du Green IT. Il propose une méthodologie pour évaluer les impacts environnementaux directs des systèmes d'information, mettant en avant l'ACV comme un outil essentiel pour quantifier ces externalités. L'article recommande divers indicateurs environnementaux, notamment le changement climatique et l'appauvrissement de la couche d'ozone, et encourage une approche matricielle pour évaluer le cycle de vie de l'ensemble des composants des systèmes informatiques. En somme, l'article met en avant l'ACV comme un instrument essentiel pour orienter les stratégies Green IT vers une meilleure durabilité environnementale.",
-            auteur: 'Review par : Flavien GAUTIER'
+            auteur: 'Flavien GAUTIER'
         },
         {
             titre: 'Green IT : écoresponsabilité des APIs encouragée par le nouveau label API Green Score',
@@ -128,7 +128,7 @@ function Reference() {
                         </div>
                         <div className="reference-link-author">
                             <Link to={`/reference${index + 1}`}>Découvrir</Link>
-                            <span className="reference-author">{reference.auteur}</span>
+                            <span className="reference-author">Review par : {reference.auteur}</span>
                         </div>
                     </div>
                 ))}
--- Front end/front-end/src/pages/references/Reference14.js
+++ Front end/front-end/src/pages/references/Reference14.js
@@ -19,30 +19,30 @@ function Reference14() {
             <section id="article-section">
                 <h1>0. Auteurs de l'article</h1>
                 <span>L'article a été réalisé par les organisations </span>
-                <a href="https://www.greenit.fr/" target="_blank" rel="noopener noreferrer" className="site-link-author">GreenIT.fr</a><a>, </a>
-                <a href="https://www.espelia.fr/#/" target="_blank" rel="noopener noreferrer" className="site-link-author">Espelia</a><a> et </a>
-                <a href="https://resilio-solutions.com/" target="_blank" rel="noopener noreferrer" className="site-link-author">Resilio</a><a> pour le </a>
-                <a href="https://club.greenit.fr/" target="_blank" rel="noopener noreferrer" className="site-link-author">Club Green IT</a><a> et à porté sur 10 grandes entreprises privées et publiques, 39 760 utilisateurs et 206 475 équipements numériques (informatiques et télécoms).</a>
+                <a href="https://www.greenit.fr/" target="_blank" rel="noopener noreferrer" className="site-link-author">GreenIT.fr</a><span>, </span>
+                <a href="https://www.espelia.fr/#/" target="_blank" rel="noopener noreferrer" className="site-link-author">Espelia</a><span> et </span>
+                <a href="https://resilio-solutions.com/" target="_blank" rel="noopener noreferrer" className="site-link-author">Resilio</a><span> pour le </span>
+                <a href="https://club.greenit.fr/" target="_blank" rel="noopener noreferrer" className="site-link-author">Club Green IT</a><span> et à porté sur 10 grandes entreprises privées et publiques, 39 760 utilisateurs et 206 475 équipements numériques (informatiques et télécoms).</span>
             </section>
             <section id="article-section">
                 <h1>1. Méthodologie ACV</h1>
-                <a>L'article met en évidence l'importance de l'Analyse du Cycle de Vie (ACV) pour évaluer l'impact environnemental des systèmes d'information. L'ACV est une méthode multicritère qui évalue les impacts environnementaux directs, mais dans ce contexte, elle se limite aux ACV attributionnelles.</a>
+                <p>L'article met en évidence l'importance de l'Analyse du Cycle de Vie (ACV) pour évaluer l'impact environnemental des systèmes d'information. L'ACV est une méthode multicritère qui évalue les impacts environnementaux directs, mais dans ce contexte, elle se limite aux ACV attributionnelles.</p>
             </section>
             <section id="article-section">
                 <h1>2. Cycle de Vie Matriciel</h1>
-                <a>L'article propose de passer d'une approche circulaire à une approche matricielle pour évaluer le cycle de vie de l'ensemble des équipements (terminaux, réseaux, datacenters) contribuant au fonctionnement des systèmes d'information. Cela permet d'éviter les transferts de pollution d'une phase à l'autre et d'un tiers à l'autre.</a>
+                <p>L'article propose de passer d'une approche circulaire à une approche matricielle pour évaluer le cycle de vie de l'ensemble des équipements (terminaux, réseaux, datacenters) contribuant au fonctionnement des systèmes d'information. Cela permet d'éviter les transferts de pollution d'une phase à l'autre et d'un tiers à l'autre.</p>
             </section>
             <section id="article-section">
                 <h1>3. Indicateurs Environnementaux</h1>
-                <a>L'article recommande plusieurs indicateurs environnementaux pour évaluer les impacts, notamment le changement climatique, l'appauvrissement de la couche d'ozone, l'émission de particules, l'acidification, l'utilisation des ressources, et d'autres. Ces indicateurs permettent de quantifier les externalités environnementales.</a>
+                <p>L'article recommande plusieurs indicateurs environnementaux pour évaluer les impacts, notamment le changement climatique, l'appauvrissement de la couche d'ozone, l'émission de particules, l'acidification, l'utilisation des ressources, et d'autres. Ces indicateurs permettent de quantifier les externalités environnementales.</p>
             </section>
             <section id="article-section">
                 <h1>4. Stratégie Green IT</h1>
-                <a>L'ACV est présentée comme un outil puissant pour aider à établir une stratégie Green IT, identifier des leviers d'amélioration significatifs et communiquer de manière objective sur les performances et les améliorations environnementales.</a>
+                <p>L'ACV est présentée comme un outil puissant pour aider à établir une stratégie Green IT, identifier des leviers d'amélioration significatifs et communiquer de manière objective sur les performances et les améliorations environnementales.</p>
             </section>
             <section id="article-section">
                 <h1>5. Conclusion</h1>
-                <a>L'article "Benchmark Green IT 2022" met en avant l'importance de l'Analyse du Cycle de Vie (ACV) dans l'évaluation des impacts environnementaux des systèmes d'information, contribuant ainsi au Green IT. Il propose une méthodologie, des indicateurs environnementaux, et encourage une approche matricielle du cycle de vie pour une évaluation complète. L'ACV est reconnue comme un outil clé pour piloter une stratégie Green IT au niveau de l'entreprise.</a>
+                <p>L'article "Benchmark Green IT 2022" met en avant l'importance de l'Analyse du Cycle de Vie (ACV) dans l'évaluation des impacts environnementaux des systèmes d'information, contribuant ainsi au Green IT. Il propose une méthodologie, des indicateurs environnementaux, et encourage une approche matricielle du cycle de vie pour une évaluation complète. L'ACV est reconnue comme un outil clé pour piloter une stratégie Green IT au niveau de l'entreprise.</p>
             </section>
         </div>
     );
